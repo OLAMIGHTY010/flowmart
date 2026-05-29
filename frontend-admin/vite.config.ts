eba2d23b47e97a3ed678f40798d90ebd52478373
Emarkees <emintechsolutions@gmail.com>
2026-05-29 19:36:41 +0100

    Update vite.config files

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+
+      devOptions: {
+        enabled: true
+      },
+
+      manifest: {
+        name: 'Flowmart',
+        short_name: 'Flowmart Admindashboard',
+        description: 'Admin dashboard - Your One-Stop Shop',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/hero.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/hero.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
