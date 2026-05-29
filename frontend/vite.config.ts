f4c4a5d2f6eca354fb7b51533afcde9408d98237
Emarkees <emintechsolutions@gmail.com>
2026-05-29 09:56:22 +0100

    Initiate setup, install tailwind css

---
+++ frontend/vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+
+      devOptions: {
+        enabled: true
+      },
+
+      manifest: {
+        name: 'Flowmart',
+        short_name: 'Flowmart',
+        description: 'Flowmart - Your One-Stop Shop',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+
+        icons: [
+          {
+            src: '/hero.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/hero.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
