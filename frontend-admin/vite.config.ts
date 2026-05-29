af4901459ac442e0835423b41e5bf1eff7531e00
Emarkees <emintechsolutions@gmail.com>
2026-05-29 21:23:08 +0100

    Update login page

--- frontend-admin/vite.config.ts
+++ frontend-admin/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
 
 export default defineConfig({
   plugins: [
@@ -37,5 +39,10 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
