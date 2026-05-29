6f3526e6be3281f7c9687a3b024960f7a6bf4983
Emarkees <emintechsolutions@gmail.com>
2026-05-29 20:29:13 +0100

    Install ui-sshadcn files

--- frontend-admin/src/pages/Login.tsx
+++ frontend-admin/src/pages/Login.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center">
-            <h1>Login</h1>
+            <Card className="w-full max-w-md shawdow-lg">
+                <CardHeader className="text-center">
+                    <img
+                        src="./logo.png"
+                        className="mx-auto mb-4 flex h-16 w-16 items-center justify-center rounded-xl"
+                    />
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="space-y-2">
+                            <label   htmlFor="email">Email</label>
+                            <input id="email" type="email" />
+                        </div>
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <Label htmlFor="password">Password</Label>
+                                <Link to="/forgot-password" className="text-xs text-primary hover:underline">
+                                    Forgot password?
+                                </Link>
+                            </div>
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="••••••••"
+                            />
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 }
