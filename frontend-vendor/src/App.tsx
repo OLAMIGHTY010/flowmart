105814118eecf747ba02cbae9c2e66c41cbe03b0
Emarkees <emintechsolutions@gmail.com>
2026-06-04 05:42:08 +0100

    Backup before Tailwind v4 upgrade

--- frontend-vendor/src/App.tsx
+++ frontend-vendor/src/App.tsx
@@ -1,5 +1,5 @@
 
-import './index.css'
+import './App.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Login from "@/pages/Login";
