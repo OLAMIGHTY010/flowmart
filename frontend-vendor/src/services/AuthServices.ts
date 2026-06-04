31c4ce8a7f2ddbbbb291bef8427adc126229ba4a
aolusola <olusolaadeyemi8@gmail.com>
2026-06-04 15:49:56 +0100

    Add authServices file

--- frontend-vendor/src/services/AuthServices.ts
+++ frontend-vendor/src/services/AuthServices.ts
@@ -1,11 +1,14 @@
-import type { LoginRequest } from "@/types/api";
+import type { ApiResponse, LoginRequest, LoginResponse } from "@/types/api";
 import { apiClient } from "./api";
 
 export const authService = {
-    initSession: () => apiClient.initSession(),
+  // 1. Defined on authService using a comma (,) at the end instead of a semicolon (;)
+  initSession: (): Promise<void> => apiClient.post("/auth/init"),
 
-    login: async(data: LoginRequest) => {
-        await apiClient
-        return apiClient.get.post<ApiResponse<LoginResponse>>("/login", data)
-    }
-}
+  login: async (data: LoginRequest): Promise<ApiResponse<LoginResponse>> => {
+    await authService.initSession(); // Or use: await this.initSession();
+    return apiClient.post<ApiResponse<LoginResponse>>("/login", data);
+  },
+
+  logout: () => apiClient.post<ApiResponse<null>>("/logout")
+};
