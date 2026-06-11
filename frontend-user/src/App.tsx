aa8da6449b57d5b7bc407a5e7f3b1ad9733d3742
Emarkees <emintechsolutions@gmail.com>
2026-06-11 08:07:33 +0100

    Update index.css file, App.tsx file

--- frontend-user/src/App.tsx
+++ frontend-user/src/App.tsx
@@ -2,22 +2,10 @@ import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// import Login from "@/pages/Login";
 import Homepage from "./pages/Homepage";
-import Register from "./pages/SignUp";
-// import Register from "@/pages/Register";
-// import VerifyOtpPage from "@/pages/VerifyOtpPage";
-// import ProfileSetup from "@/pages/ProfileSetup";
-// import KYCInfo from "@/pages/KYCInfo";
-// import KYCSubmit from "@/pages/KYCSubmit";
-// import KYCReview from "@/pages/KYCReview";
-// import KYCVerification from "@/pages/KYCVerification";
-// import Dashboard from "@/pages/Dashboard";
+import Register from "./pages/Register";
+import AppLayout from "./components/AppLayout";
 
-// import { createProtectedRoute } from "@/routes/guards/ProtectedRoute";
-// import { OnboardingGuard } from "@/routes/guards/OnboardingGuard";
-
-// React Query
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -33,35 +21,24 @@ const queryClient = new QueryClient({
   },
 });
 
-// const ProtectedRoute = createProtectedRoute();
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-
-          {/* PUBLIC */}
-          <Route path="/" element={<Homepage />} />
+          {/* Pages WITHOUT Navbar/Layout */}
           <Route path="/register" element={<Register />} />
-          {/* <Route path="/register" element={<Register />} />
-          <Route path="/otp" element={<VerifyOtpPage />} /> */}
 
-          {/* AUTH ONLY (must be logged in) */}
-          {/* <Route element={<ProtectedRoute />}>
-            <Route path="/profile-setup" element={<ProfileSetup />} />
-          </Route> */}
+          {/* Pages WITH Layout */}
+          <Route
+            path="/"
+            element={
+              <AppLayout>
+                <Homepage />
+              </AppLayout>
+            }
+          />
 
-          {/* ONBOARDING FLOW (profile → kyc → dashboard rules) */}
-          {/* <Route element={<OnboardingGuard />}>
-            <Route path="/kyc" element={<KYCInfo />} />
-            <Route path="/kyc/submit" element={<KYCSubmit />} />
-            <Route path="/kyc/review" element={<KYCReview />} />
-            <Route path="/kyc/verification" element={<KYCVerification />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route> */}
-
-          {/* fallback */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
