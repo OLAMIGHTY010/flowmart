0fe97e78896c7261ffec7ac4c68c4d17544a139f
Emarkees <emintechsolutions@gmail.com>
2026-06-13 05:33:31 +0100

    Update useProducts, add useVendorProducts files

--- frontend-user/src/hooks/useProducts.ts
+++ frontend-user/src/hooks/useProducts.ts
@@ -2,8 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 import { productServices } from "@/services/ProductServices";
 
 export const useProducts = () => {
-    return useQuery({
-        queryKey: ["products"],
-        queryFn: async () => await productServices.getProducts(),
-    })
-}
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const products = await productServices.getProducts();
+      return Array.isArray(products) ? products : [];
+    },
+  });
+};
